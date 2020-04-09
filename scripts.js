e3836ed304ee0d4db1704367856fb1407c82f5e1
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2020-04-09 00:08:33 -0500

    Se creo el metodo de listar Links consumiendo el api

--- scripts.js
+++ scripts.js
@@ -4,6 +4,19 @@ let listarLinks = () => {
     axios.get("http://localhost:3000/url").then(respuesta => {
         console.log("Respuesta del Api")
         console.log(respuesta.data)
+        let data = ""
+        let lista = document.getElementById("listaLinks")
+        for(let i = 0; i < respuesta.data.length; i++){
+            let link = respuesta.data[i]
+            data += "<tr>"
+            data += `<td>${link.url}</td>`
+            data += `<td>${link.nombre}</td>`
+            data += `<td>${link.descripcion}</td>`
+            data += `<td><button type="button" onclick = "cargarDatos(${link.idlink})" class="btn btn-primary">Editar</button></td>`
+            data += `<td><button type="button" onclick = "eliminarLink(${link.idlink})" class="btn btn-primary">Eliminar</button></td>`
+            data += "</tr>"
+        }
+        lista.innerHTML = data;
     }).catch(error => {
         console.log(error)
     })
